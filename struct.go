5683de8e7cdd64712c65adc2f798725645edc885
Hassie <hassieswift621@users.noreply.github.com>
2021-07-26 12:22:08 +0100

    Add subscription fields to user struct

--- struct.go
+++ struct.go
@@ -1,5 +1,10 @@
 package tatsu_api
 
+import (
+	"golang.org/x/xerrors"
+	"time"
+)
+
 type GuildMemberRanking struct {
 	GuildID string `json:"guild_id"`
 	Rank    int64  `json:"rank"`
@@ -18,15 +23,28 @@ type GuildRanking struct {
 	UserID string `json:"user_id"`
 }
 
+type Timestamp string
+
+func (t Timestamp) Parse() (time.Time, error) {
+	timestamp, err := time.Parse(time.RFC3339, string(t))
+	if err != nil {
+		return time.Time{}, xerrors.Errorf("tatsu_api: parse subscription renewal: %w", err)
+	}
+
+	return timestamp, nil
+}
+
 type User struct {
-	AvatarURL     string `json:"avatar_url"`
-	Credits       uint64 `json:"credits"`
-	Discriminator string `json:"discriminator"`
-	ID            string `json:"id"`
-	InfoBox       string `json:"info_box"`
-	Reputation    uint64 `json:"reputation"`
-	Title         string `json:"title"`
-	Tokens        uint64 `json:"tokens"`
-	Username      string `json:"username"`
-	XP            uint64 `json:"xp"`
+	AvatarURL           string    `json:"avatar_url"`
+	Credits             uint64    `json:"credits"`
+	Discriminator       string    `json:"discriminator"`
+	ID                  string    `json:"id"`
+	InfoBox             string    `json:"info_box"`
+	Reputation          uint64    `json:"reputation"`
+	SubscriptionRenewal Timestamp `json:"subscription_renewal"`
+	SubscriptionType    uint8     `json:"subscription_type"`
+	Title               string    `json:"title"`
+	Tokens              uint64    `json:"tokens"`
+	Username            string    `json:"username"`
+	XP                  uint64    `json:"xp"`
 }
