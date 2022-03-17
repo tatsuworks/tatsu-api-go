c92258586082328fa00a31b878d3c444e7853583
Hassie <hassie@tatsu.works>
2022-03-17 12:00:44 +0000

    Add get guild member points endpoint

--- client_guilds.go
+++ client_guilds.go
@@ -6,6 +6,22 @@ import (
 	"golang.org/x/xerrors"
 )
 
+// GetGuildMemberPoints wraps around GetGuildMemberPointsWithContext using the background context.
+func (c *Client) GetGuildMemberPoints(guildID string, userID string) (*GuildMemberPoints, error) {
+	return c.GetGuildMemberPointsWithContext(context.Background(), guildID, userID)
+}
+
+// GetGuildMemberPointsWithContext gets a guild member's points using the specified context.
+func (c *Client) GetGuildMemberPointsWithContext(ctx context.Context, guildID string,
+	userID string) (*GuildMemberPoints, error) {
+	points, err := c.rc.getGuildMemberPoints(ctx, guildID, userID)
+	if err != nil {
+		return nil, xerrors.Errorf("failed to get get guild member points: %w", err)
+	}
+
+	return points, nil
+}
+
 // ModifyGuildMemberScore wraps around ModifyGuildMemberScore using the background context.
 func (c *Client) ModifyGuildMemberScore(guildID string, userID string, action Action,
 	amount uint32) (*GuildMemberScore, error) {
--- endpoint_guilds.go
+++ endpoint_guilds.go
@@ -2,6 +2,10 @@ package tatsu_api
 
 import "fmt"
 
+func getGuildMemberPoints(guildID string, userID string) string {
+	return baseURL + fmt.Sprintf("/guilds/%s/members/%s/points", guildID, userID)
+}
+
 func modifyGuildMemberScore(guildID string, userID string) string {
 	return baseURL + fmt.Sprintf("/guilds/%s/members/%s/score", guildID, userID)
 }
